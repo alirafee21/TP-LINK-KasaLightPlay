61c167df9e9591e710f9e3d6d34446357c50a82e
Rafee Ali <90751182+alirafee21@users.noreply.github.com>
2023-04-05 08:13:12 -0400

    toggle added

--- Kasa-Test.py
+++ Kasa-Test.py
@@ -1,18 +1,31 @@
 import asyncio
 from kasa import *
-# from pyHS100.smartdevice import SmartDeviceException
+
 from kasa.exceptions import SmartDeviceException
+
+
 # found_devices = asyncio.run(Discover.discover())
 # address = []
 # for dev in found_devices:
 #     address.append(dev)
 # if address:
 
+def toggle_light():
+    try:
+        bulb = SmartBulb("192.168.2.238")
+        asyncio.run(bulb.update())
+        # asyncio.run(bulb.turn_off())
+        state = bulb.is_on
+        if state:
+             asyncio.run(bulb.turn_off())
+        else:
+            asyncio.run(bulb.turn_on())
+        #
+        # asyncio.run(bulb.turn_on())
+    except SmartDeviceException as e:
+        print(e)
 
-try:
-    bulb = SmartBulb("192.168.2.238")
-    asyncio.run(bulb.update())
-    # asyncio.run(bulb.turn_off())
-    asyncio.run(bulb.turn_on())
-except SmartDeviceException as e:
-    print(e)
+toggle_light()
+
+# if __name__ == "main":
+#     toggle_light()
